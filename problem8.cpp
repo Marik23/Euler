cf388094a97293b5ded5947f4bd30dbf9c47addc
Marik23 <bdk_marik@web.de>
2013-11-11 16:38:25 +0000

    Improved code for problem 8

--- problem8.cpp
+++ problem8.cpp
@@ -5,46 +5,29 @@
 int main()
 {
     std::string number = "7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450";
-    std::vector<std::string> numbersWithoutZeros;
-    numbersWithoutZeros.push_back("");
+
     int numberOfConsDigits = 5;
     int numberOfConsDigitsDecr = numberOfConsDigits - 1;
     int numberDigits = number.size();
     
-    // Split with delimeter '0'
-    for (int i = 0; i < numberDigits; ++i)
-    {
-	if (number[i] == '0')
-	{
-	    if (numbersWithoutZeros.back().size() < numberOfConsDigits)
-		numbersWithoutZeros.pop_back();
-	    numbersWithoutZeros.push_back("");
-	    continue;
-	}
-
-	// Add to new number
-	numbersWithoutZeros.back() += number[i];
-
-    }
-
-    if (numbersWithoutZeros.back().size() < numberOfConsDigits)
-	numbersWithoutZeros.pop_back();
-
     int largestProduct = 0;
     int currentProduct;
-    int size;
-    for (std::vector<std::string>::iterator it = numbersWithoutZeros.begin(); it != numbersWithoutZeros.end(); ++it)
+    
+    for (int i = numberOfConsDigitsDecr; i < numberDigits; ++i)
     {
-	size = (*it).size();
-	for (int i = numberOfConsDigitsDecr; i < size; ++i)
-	{
-	    currentProduct = 1;
-	    for (int j = i - numberOfConsDigitsDecr; j <= i; ++j)
-		currentProduct *= ((*it)[j] - '0');
-
+        if (number[i] == '0')
+        {
+            i += numberOfConsDigits;
+            continue;
+        }
+        
+        currentProduct = 1;
+        
+        for (int j = i - numberOfConsDigitsDecr; j <= i; ++j)
+            currentProduct *= (number[j] - '0');
+            
 	    if (currentProduct > largestProduct)
-		largestProduct = currentProduct;
-	}
+            largestProduct = currentProduct;
     }
  
     std::cout << "Largest product: " << largestProduct << std::endl;
