be156ba4a66b50fea5fd1765f98f332ea3d215dc
Marik23 <bdk_marik@web.de>
2013-11-21 10:10:04 +0000

    Improved problem 21

--- problem21.cpp
+++ problem21.cpp
@@ -1,13 +1,11 @@
 #include <iostream>
 #include <cmath>
-#include <map>
 
 int main()
 {
     int limit = 10000;
     int sum = 0;
-    std::map<int, int> sums;
-    std::multimap<int, int> sumsInverse;
+    int sums[limit];
     int sumDivisors;
     int sqrtNum;
     for (int i = 1; i < limit; ++i)
@@ -21,19 +19,17 @@ int main()
 		sumDivisors += i/j;
 	    }
 	sums[i] = sumDivisors;
-	sumsInverse.insert(std::pair<int, int>(sumDivisors, i));
     }
 
-
-    std::pair<std::multimap<int, int>::iterator, std::multimap<int, int>::iterator> values;
+    int currentDivisors;
     for (int i = 1; i < limit; ++i)
     {
-	values = sumsInverse.equal_range(i);
-	for (std::multimap<int, int>::iterator it = values.first; it != values.second; ++it)
-	    if (it->second == sums[i] && it->second != it->first)
-	    {
+	currentDivisors = sums[i];
+	if (currentDivisors != i)
+	{
+	    if (currentDivisors < limit && i == sums[currentDivisors])
 		sum += i;
-	    }
+	}
     }
 
     std::cout << "Sum: " << sum << std::endl;
